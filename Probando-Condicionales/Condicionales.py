c040f406e9e51630d8134b62d859e130e3552bd1
angelsanchezh <angelsanchezh@gmail.com>
2022-10-21 18:29:05 -0500

    condicion

--- Probando-Condicionales/Condicionales.py
+++ Probando-Condicionales/Condicionales.py
@@ -1,7 +1,16 @@
+Nombre = input('como te llamas ')
+print('Hola ' + Nombre )
+
+edad = int(input('cuantos años tienes '))
+masDe12 = edad >= 12
+
+respuesDelHijoDelDueño = input('eres hijo del dueño')
+esHijoDelDueño= respuesDelHijoDelDueño == 'si'
+
+puedesPasar = masDe12 or esHijoDelDueño
 
 
-edad = 10
-if edad >= 18:
-    print('es mayor de edad')
+if puedesPasar:
+    print('usted puede pasar')
 else:
     print('no puedes ingresar por ser menor de edad')
